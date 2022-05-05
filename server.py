2a66faaf46adf9b0e25194a3113ff47e1adef1e2
SawshaDev <contactcallimarie@gmail.com>
2022-05-05 23:22:50 +0000

    added script + moved images to new folder

--- server.py
+++ server.py
@@ -1,21 +1,32 @@
-from quart import Quart, jsonify, send_from_directory, url_for, request
+from quart import Quart, jsonify, send_from_directory, url_for, request, render_template
 import random
 import os
 
 app = Quart(__name__)
 
+@app.route("/")
+async def home():
+    choice = random.choice(os.listdir("/root/yanpdb/nsfw_cdn/images/helltakerpics"))
+    image = os.path.join("/root/yanpdb/nsfw_cdn/images/helltakerpics", choice)
+    raw_image = url_for('sendfile',filename=choice)
+    
+    return await render_template('index.html', host=request.host, raw=raw_image)
+
+    
+    
+
 @app.route('/imgs/<filename>')
 async def sendfile(filename=None):
-    dir = "/root/yanpdb/nsfw_cdn/images"
+    dir = "/root/yanpdb/nsfw_cdn/images/helltakerpics"
     image = os.path.join(dir, filename)
-
+    
     return await send_from_directory(dir,filename)
 
 
 @app.route('/helltaker')
 async def helltaker():
-    choice = random.choice(os.listdir("/root/yanpdb/nsfw_cdn/images"))
-    image = os.path.join("/root/yanpdb/nsfw_cdn/images", choice)
+    choice = random.choice(os.listdir("/root/yanpdb/nsfw_cdn/images/helltakerpics"))
+    image = os.path.join("/root/yanpdb/nsfw_cdn/images/helltakerpics", choice)
     raw_image = url_for('sendfile',filename=choice)
     return jsonify(url=f"https://{request.host}{raw_image}")
 
