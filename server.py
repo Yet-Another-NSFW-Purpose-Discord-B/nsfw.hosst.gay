f12515d3867a8db53f6b098f7712e24ffc137e65
SawshaDev <contactcallimarie@gmail.com>
2022-05-05 12:32:02 +0000

    switched to async/quart and made support for pm2

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
-from flask import Flask, jsonify, send_from_directory, url_for, request
+from quart import Quart, jsonify, send_from_directory, url_for, request
 import random
 import os
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 @app.route('/imgs/<filename>')
-def sendfile(filename=None):
+async def sendfile(filename=None):
     dir = "/root/yanpdb/nsfw_cdn/images"
     image = os.path.join(dir, filename)
 
-    return send_from_directory(dir,filename)
+    return await send_from_directory(dir,filename)
 
 
 @app.route('/helltaker')
-def helltaker():
+async def helltaker():
     choice = random.choice(os.listdir("/root/yanpdb/nsfw_cdn/images"))
     image = os.path.join("/root/yanpdb/nsfw_cdn/images", choice)
     raw_image = url_for('sendfile',filename=choice)
     return jsonify(url=f"https://{request.host}{raw_image}")
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=2030)
+
+app.run(debug=True, port=2030)
